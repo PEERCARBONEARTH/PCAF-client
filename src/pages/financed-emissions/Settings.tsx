7e5415b4e4a4b3eda3520bbe29a3443ffe2b7ace
0xBharaj Sir_heeb <raymond@peercarbon.earth>
2025-09-03 10:03:18 +0300

    Update Settings.tsx

--- src/pages/financed-emissions/Settings.tsx
+++ src/pages/financed-emissions/Settings.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+https://pcaf-client.vercel.app/financed-emissions/uploadimport { useState } from 'react';
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
 import { Button } from '@/components/ui/button';
 import { Badge } from '@/components/ui/badge';
